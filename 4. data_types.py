88ba59ecaf3d28a5a77ae783ebbf591556ad737a
Yash Patel <mailforyash04@gmail.com>
2022-05-23 23:35:17 +0530

    Data types in python

---
+++ 4. data_types.py
@@ -0,0 +1,17 @@
+x = 21 # INT
+print(type(x))
+
+y = 11.22 # FLOAT
+print(type(y))
+
+a = 55j # COMPLEX
+print(type(a))
+
+b = True # BOOL
+print(type(b))
+
+c = b"hello"
+print(type(c))
+
+d = None
+print(type(d))
