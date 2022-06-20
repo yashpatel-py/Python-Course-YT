b2f016590b448cb96399e715f95eae1b922fbcf6
Yash Patel <mailforyash04@gmail.com>
2022-06-20 10:56:45 +0530

    String methods part 1

---
+++ 14. string methods part 1.py
@@ -0,0 +1,8 @@
+a = "apple apple apple apple Banana Mango"
+b = "apple"
+# print(a.casefold())
+# print(a.capitalize())
+# print(a.count('apple'))
+# print(b.center(20, 'D'))
+# print(a.endswith('p'))
+print(a.find('p'))
