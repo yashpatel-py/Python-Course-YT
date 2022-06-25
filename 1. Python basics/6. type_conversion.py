e412260fa7ea8613e036649be2313788a8438b0b
Yash Patel <mailforyash04@gmail.com>
2022-06-25 13:01:20 +0530

    Python Basich Done

---
+++ 1. Python basics/6. type_conversion.py
@@ -0,0 +1,40 @@
+a = 3 # int
+b = 3.1 # float
+c = "Django World" # str
+d = "5" # str
+
+# print(d)
+# print(type(d))
+
+e = "5.1" # str
+f = 5j # complex
+
+# int to float
+f_to_i = float(3)
+print(f_to_i)
+print(type(f_to_i))
+
+# Float to int
+i_to_f = int(b)
+print(i_to_f)
+print(type(i_to_f))
+
+# str to int
+s_to_i = int(d)
+print(s_to_i)
+print(type(s_to_i))
+
+# str to float
+s_to_f = float(e)
+print(s_to_f)
+print(type(s_to_f))
+
+# int to complex
+i_to_c = complex(a)
+print(i_to_c)
+print(type(i_to_c))
+
+# complex to int
+c_to_i = str(f)
+print(c_to_i)
+print(type(c_to_i))
---
+++ 1. Python basics/7. user_input.py
@@ -0,0 +1,5 @@
+# a = input("Enter number: ")
+
+b = str(input("Enter number: "))
+print("Name you endered is", b)
+print(type(b))
