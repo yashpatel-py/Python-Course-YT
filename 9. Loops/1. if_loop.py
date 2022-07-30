9c0282d95a82dd1d5fb485d8a31641faa206626b
Yash Patel <mailforyash04@gmail.com>
2022-07-30 23:18:43 +0530

    If loop

---
+++ 9. Loops/1. if_loop.py
@@ -0,0 +1,34 @@
+a = 10
+b = 15
+c = 10
+
+# if a > b:
+#     print('a > b')
+
+# elif a==b:
+#     print('a is equal to C')
+
+# elif b > 14:
+#     print('b is grater than 14')
+
+# else:
+#     print('b > a')
+
+# short hand if loop
+# print('A') if a > b else print('==') if a==c else print('b > a pr c')
+
+# if loop using logical operator
+# if 5>3 and 5<10:
+#     print("Yes")
+# else:
+#     print("No")
+
+# Nested if loop
+if 11 > 10:
+    print('11 is greter than 10')
+    if 11 == 11:
+        print('Yes')
+    else:
+        print('No')
+else:
+    print('11 > 10')
