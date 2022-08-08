742d9cf7428cb7d8d3572fd7a23367ae0a916185
Yash Patel <mailforyash04@gmail.com>
2022-08-08 20:50:16 +0530

    Function in python

---
+++ 10. Function in python/1. function_in_python.py
@@ -0,0 +1,22 @@
+# def my_func(name, age):
+#     print('My name is ' + name + ", and i am " + age + " year old")
+
+# def my_func2(name, age):
+#     print('My name is ' + name + ", and i am " + age + " year old")
+
+# my_func2("Yash Patel", "23")
+# my_func("Manav", "21")
+
+# -------------------------------------------------
+
+# def details(*name):
+#     print('My name is ', name[2])
+#     print(type(name))
+
+# details('yash', 'parth', 'manav', 'dhruv')
+
+
+def details2(**name):
+    pass
+
+details2(f_name = "yash", l_name="Patel")
