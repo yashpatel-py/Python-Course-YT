ed16d6c8fbce1e95729f2b6c7b3395b939264b2b
Yash Patel <mailforyash04@gmail.com>
2022-08-16 14:49:36 +0530

    Math in python

---
+++ 12. Math in python/2. math_module.py
@@ -0,0 +1,17 @@
+import math
+
+# --------------- math.sqrt() ---------------
+# a = math.sqrt(64)
+# print(a)
+
+# -------------- math.ceil() ----------------
+# b = math.ceil(3.2) # Upword
+# print(b)
+
+# ------------- math.floor() ----------------
+# c = math.floor(3.2) # downword
+# print(c)
+
+# ------------- math.pi -------------------
+d = math.pi
+print(d)
