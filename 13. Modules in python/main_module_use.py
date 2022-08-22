7849423264b41b2ea35650d59ca4a841fb4965aa
Yash Patel <mailforyash04@gmail.com>
2022-08-22 15:17:43 +0530

    Module in python

---
+++ 13. Modules in python/main_module_use.py
@@ -0,0 +1,4 @@
+import module_main
+
+module_main.full_name("Yash", "Patel")
+module_main.age_check(21)
---
+++ 13. Modules in python/module_main.py
@@ -0,0 +1,8 @@
+def full_name(f_name, l_name):
+    print(f_name, l_name)
+
+def age_check(age):
+    if age >= 18:
+        print("You are allowed in the club")
+    else:
+        print("Go home and drink milk")
