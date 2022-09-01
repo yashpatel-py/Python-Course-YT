ee3337ef91f3aeead02e810017db62d4a1a08abc
Yash Patel <mailforyash04@gmail.com>
2022-09-01 17:16:11 +0530

    Date and time

---
+++ 14. Date and time/1. Date_time.py
@@ -0,0 +1,17 @@
+import datetime
+
+# current - full date and time
+# a = datetime.datetime.now()
+# print(a)
+
+# current year
+# print(a.year)
+
+# current day
+# print(a.strftime("%A"))
+
+
+# custom date and time
+b = datetime.datetime(2000, 4, 21)
+# print(b)
+print(b.strftime("%B"))
